d392d76d88c62787dc7d61a03af899762b4a4921
labtorie <56982278+labtorie@users.noreply.github.com>
2020-07-01 01:43:04 +0300

    Updates

---
+++ precache-manifest.6ca85781a8b0ed9f8687407be4470b0a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2a95be94323a65cfe669542496da2d",
+    "url": "/frogs/index.html"
+  },
+  {
+    "revision": "e21faed3031a3eb00d76",
+    "url": "/frogs/static/css/main.ee8d3415.chunk.css"
+  },
+  {
+    "revision": "7c34d68bacb6ce61385d",
+    "url": "/frogs/static/js/2.1dabe5db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frogs/static/js/2.1dabe5db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e21faed3031a3eb00d76",
+    "url": "/frogs/static/js/main.090e0cb4.chunk.js"
+  },
+  {
+    "revision": "59d74674d68d1197462d",
+    "url": "/frogs/static/js/runtime-main.36dc2fd3.js"
+  },
+  {
+    "revision": "2524462da1f86937b30a5ed7ec15bea6",
+    "url": "/frogs/static/media/bkg.2524462d.png"
+  },
+  {
+    "revision": "64ca5c54ec388af01729e2f9b6be3d2e",
+    "url": "/frogs/static/media/logo.64ca5c54.png"
+  },
+  {
+    "revision": "690950e2d9c57c6bb950fd9428444ddb",
+    "url": "/frogs/static/media/lol.690950e2.jpg"
+  },
+  {
+    "revision": "e887d8a8aba6604d8257d759eaa43ce1",
+    "url": "/frogs/static/media/pond.e887d8a8.png"
+  }
+]);
